62ed89f72ea21ad3c4764c535c7b9a3f49fe2fae
Ethan Tanen <e_tanen@coloradocollege.edu>
2018-05-28 17:04:48 -0400

    more attempts..

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'mysite/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'mysite/static')
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
