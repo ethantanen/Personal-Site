45dd4a9e83bd904d57fe82ee34d917d8aa090d9c
Ethan Tanen <e_tanen@coloradocollege.edu>
2018-06-03 23:08:25 -0400

    static files work again

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,12 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
 
 # Extra places for collectstatic to find static files.
-'''
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),)
-'''
